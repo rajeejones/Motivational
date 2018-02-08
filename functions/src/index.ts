3e33b0b35a17e21026fe912bb86db848f30e497d
Rajee Jones <jones.rajee@gmail.com>
2018-02-08 00:35:37 -0600

    Update index.ts
    Removed unused lines of code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,6 @@
 // Cloud Functions for Firebase SDK 
 import * as functions from 'firebase-functions';
 
-// Configure Express
-const express = require('express');
-
-// Allow CORS
-const cors = require('cors')({origin: true});
-const cookieParser = require('cookie-parser')();
-const app = express();
-
 // Firebase Admin SDK to access Firebase Realtime Database
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
@@ -24,45 +16,6 @@ const twilioNumber = '+14697784483';
 
 // Uses Cron to send messages everyday
 //const cronJob = require('cron').CronJob;
-
-const validateFirebaseIdToken = (req, res, next) => {
-    console.log('Check if request is authorized with Firebase ID token');
-  
-    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
-        !req.cookies.__session) {
-      console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-          'Make sure you authorize your request by providing the following HTTP header:',
-          'Authorization: Bearer <Firebase ID Token>',
-          'or by passing a "__session" cookie.');
-      res.status(403).send('Unauthorized');
-      return;
-    }
-  
-    let idToken;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-      console.log('Found "Authorization" header');
-      // Read the ID Token from the Authorization header.
-      idToken = req.headers.authorization.split('Bearer ')[1];
-    } else {
-      console.log('Found "__session" cookie');
-      // Read the ID Token from cookie.
-      idToken = req.cookies.__session;
-    }
-    admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
-      console.log('ID Token correctly decoded', decodedIdToken);
-      req.user = decodedIdToken;
-      next();
-    }).catch(error => {
-      console.error('Error while verifying Firebase ID token:', error);
-      res.status(403).send('Unauthorized');
-    });
-  };
-  
-  app.use(cors);
-  app.use(cookieParser);
-  app.use(validateFirebaseIdToken);
-  
-
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -116,8 +69,8 @@ export const messageSubscribers = functions.https.onRequest((request, response) 
     });
 });
 
+// Respond to Incoming Twilio messages
 export const reply = functions.https.onRequest((request, response) => {
-// app.post('/reply', (request, response) => {
 
     const twiml = new MessagingResponse();
     const message = twiml.message();
